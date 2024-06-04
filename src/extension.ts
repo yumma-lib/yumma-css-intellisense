5b85a24f110d0c2e4b3adb23dadf064152356f96
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-06-04 16:00:33 +0100

    feat: add workspaceUtilities.ts, add flex utilities

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
 import { colorUtils } from './utils/colorUtilities';
+import { isYummaInstalled } from './utils/workspaceUtilities';
 import { colors } from './yumma-css/colors';
 import { utilities } from './yumma-css/utilities';
 
@@ -10,12 +11,12 @@ colorUtils.forEach(colorClass => {
     colors.forEach(color => {
         const className = `${colorClass.prefix}${color.name}`;
         const item = new vscode.CompletionItem(className, vscode.CompletionItemKind.Color);
-        item.insertText = className;
+
         item.detail = `${colorClass.description}: ${color.color}`;
+        item.documentation = new vscode.MarkdownString(`**Utility class**: ${colorClass.description}: ${color.color}`);
+        item.insertText = className;
+
         (item as any).color = { id: 'yumma-color', color: color.color };
-
-        item.documentation = new vscode.MarkdownString(`${color.color}`);
-
         completionItems.push(item);
     });
 });
@@ -24,44 +25,57 @@ utilities.forEach(utilClass => {
     utilClass.values.forEach(value => {
         const className = `${utilClass.prefix}${value.name}`;
         const item = new vscode.CompletionItem(className, vscode.CompletionItemKind.Property);
-        item.documentation = new vscode.MarkdownString(`Utility class: ${utilClass.description}: ${value.property}`);
-        item.insertText = className;
+
         item.detail = `${utilClass.description}: ${value.property || ''}`;
+        item.documentation = new vscode.MarkdownString(`**Utility class**: ${utilClass.description}`);
+        item.insertText = className;
+
         completionItems.push(item);
     });
 });
 
-export function activate(context: vscode.ExtensionContext) {
-    const provider = vscode.languages.registerCompletionItemProvider(['css', 'html', 'javascript', 'jsx', 'tsx', 'typescript'], {
-        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-            return completionItems;
+export async function activate(context: vscode.ExtensionContext) {
+    try {
+        const hasInstalled = await isYummaInstalled();
+        if (!hasInstalled) {
+            vscode.window.showInformationMessage('YummaCSS is not installed in this workspace.');
+            return;
         }
-    });
-    
-    const hoverProvider = vscode.languages.registerHoverProvider(['css', 'html', 'javascript', 'jsx', 'tsx', 'typescript'], {
-        provideHover(document: vscode.TextDocument, position: vscode.Position) {
-            const range = document.getWordRangeAtPosition(position, /[\w-]+/);
-            if (!range) {
+
+        const provider = vscode.languages.registerCompletionItemProvider(['css', 'html', 'javascript', 'jsx', 'tsx', 'typescript'], {
+            provideCompletionItems(doc: vscode.TextDocument, position: vscode.Position) {
+                return completionItems;
+            }
+        });
+
+        const hoverProvider = vscode.languages.registerHoverProvider(['css', 'html', 'javascript', 'jsx', 'tsx', 'typescript'], {
+            provideHover(document: vscode.TextDocument, position: vscode.Position) {
+                const range = document.getWordRangeAtPosition(position, /[\w-]+/);
+                if (!range) {
+                    return null;
+                }
+                const word = document.getText(range);
+
+                const utility = utilities.find(utilClass =>
+                    utilClass.values.some(value => `${utilClass.prefix}${value.name}` === word)
+                );
+
+                if (utility) {
+                    const value = utility.values.find(value => `${utility.prefix}${value.name}` === word);
+                    if (value) {
+                        const markdownString = new vscode.MarkdownString();
+                        markdownString.appendCodeblock(`${word} {\n ${utility.description}: ${value.property || ''};\n}`, 'css');
+                        return new vscode.Hover(markdownString);
+                    }
+                }
+
                 return null;
             }
-            const word = document.getText(range);
+        });
 
-            const utility = utilities.find(utilClass =>
-                utilClass.values.some(value => `${utilClass.prefix}${value.name}` === word)
-            );
-
-            if (utility) {
-                const value = utility.values.find(value => `${utility.prefix}${value.name}` === word);
-                if (value) {
-                    const markdownString = new vscode.MarkdownString();
-                    markdownString.appendCodeblock(`${word} {\n    ${value.property || ''};\n}`, 'css');
-                    return new vscode.Hover(markdownString);
-                }
-            }
-
-            return null;
-        }
-    });
-
-    context.subscriptions.push(provider, hoverProvider);
+        context.subscriptions.push(provider, hoverProvider);
+    } catch (error) {
+        console.error("Could not activate extension due to:", error);
+        vscode.window.showErrorMessage("An error occurred while activating the extension.");
+    }
 }
