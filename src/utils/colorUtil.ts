4d2b61f950afcc0409333a159b7b4544fc8a8200
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-06-25 12:28:24 +0100

    refactor: add unreleased color utilities

--- src/utils/colorUtil.ts
+++ src/utils/colorUtil.ts
@@ -1,10 +1,13 @@
 export const colorUtils = [
-    { classPrefix: 'bg-', classValue: 'background-color' },
+    { classPrefix: 'ac-', classValue: 'accent-color' },
     { classPrefix: 'bc-', classValue: 'border-color' },
     { classPrefix: 'bc-b-', classValue: 'border-bottom-color' },
     { classPrefix: 'bc-l-', classValue: 'border-left-color' },
     { classPrefix: 'bc-r-', classValue: 'border-right-color' },
     { classPrefix: 'bc-t-', classValue: 'border-top-color' },
+    { classPrefix: 'bg-', classValue: 'background-color' },
     { classPrefix: 'cc-', classValue: 'caret-color' },
-    { classPrefix: 't-', classValue: 'color' }
+    { classPrefix: 'oc-', classValue: 'outline-color' },
+    { classPrefix: 't-', classValue: 'color' },
+    { classPrefix: 'tdc-', classValue: 'text-decoration-color' }
 ];
