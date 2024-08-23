7d4cd312cf275f2b28b5a9212915c47285e12366
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-23 21:49:46 +0100

    remove workspaceUtil.ts and update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,13 @@
 import * as vscode from "vscode";
 
-import { isYummaInstalled } from "./utilities/workspaceUtil";
 import { hoverProvider } from "./providers/hoverProvider";
 import { completionProvider } from "./providers/completionProvider";
 
 export async function activate(context: vscode.ExtensionContext) {
   try {
-    const hasInstalled = await isYummaInstalled();
-    if (!hasInstalled) {
-      vscode.window.showInformationMessage(
-        "Yumma CSS is not installed in this workspace."
-      );
-      return;
-    }
-
     context.subscriptions.push(completionProvider, hoverProvider);
-  } catch (e) {
-    console.error("Could not activate extension due to:", e);
+  } catch (ex) {
+    console.error("Could not activate extension due to:", ex);
     vscode.window.showErrorMessage(
       "An error occurred while activating the extension."
     );
