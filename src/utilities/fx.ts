a560603f67622d474c645720a37b9be3c2372d11
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-16 23:38:15 +0100

    merge effects and filters into fx category

--- src/utilities/fx.ts
+++ src/utilities/fx.ts
@@ -1,6 +1,18 @@
 import { generateClass } from "../helpers/classHelper";
 
-export const filterUtils = [
+export const fxUtils = [
+  {
+    classPrefix: "bf-b-",
+    classLink: "backdrop-filter",
+    utilityClasses: [
+      { classSuffix: "none", classValues: ["backdrop-filter: blur(0px);"] },
+      { classSuffix: "xs", classValues: ["backdrop-filter: blur(4px);"] },
+      { classSuffix: "sm", classValues: ["backdrop-filter: blur(8px);"] },
+      { classSuffix: "md", classValues: ["backdrop-filter: blur(16px);"] },
+      { classSuffix: "lg", classValues: ["backdrop-filter: blur(32px);"] },
+      { classSuffix: "xl", classValues: ["backdrop-filter: blur(64px);"] },
+    ],
+  },
   {
     classPrefix: "bs-",
     classLink: "box-shadow",
